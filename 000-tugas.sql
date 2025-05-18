ceb018903eceed1244cec64bd9de8a06fb97fce5
sisil-lia <sesilliaa31@gmail.com>
2025-05-18 22:54:48 +0700

    menambahkan file databse

---
+++ 000-tugas.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "tugas22" (
+ "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+ "deskripsi" TEXT NOT NULL,
+ "waktu" INTEGER NOT NULL
+);
+
+INSERT INTO "tugas" ("deskripsi", "waktu") VALUES
+('makan pagi', 60),
+('kuliah', 360),
+('latihan bulu tangkis', 60),
+('olah raga', 150),
+('jalan-jalan', 100),
+('berenang', 60),
+('tidur', 480);
+
