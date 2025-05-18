3ae279d2b8dc3492f4fafd6cc3037cdcb44e9c03
sisil-lia <sesilliaa31@gmail.com>
2025-05-18 23:28:48 +0700

    mengubah fungsi create

--- create.php
+++ create.php
@@ -1,18 +1,18 @@
 <?php
-...
-
+// Koneksi ke database SQLite
+try {
+    $conn = new PDO("sqlite:database.db");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
 $deskripsi = 'Tidur siang';
 $waktu = 90;
 $sql = 'INSERT INTO tugas(deskripsi, waktu) VALUES(:deskripsi, :waktu)';
-
 $statement = $conn->prepare($sql);
-
 $statement->execute([
- ':deskripsi' => $deskripsi,
- ':waktu' => $waktu
+    ':deskripsi' => $deskripsi,
+    ':waktu' => $waktu
 ]);
-
 $tugas_id = $conn->lastInsertId();
-
-... // lakkan redirect di sini
-
+echo "Tugas baru berhasil ditambahkan dengan ID: $tugas_id<br><br>";
